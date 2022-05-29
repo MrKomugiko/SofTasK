39e9f1ea19669c8572576efcec494d76d27d49ce
MrKomugiko <Mr.Komugiko@gmail.com>
2022-05-30 01:47:13 +0200

    sidebar and styling

--- SofTasK.Website/src/app/app.module.ts
+++ SofTasK.Website/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectComponent } from './project/project.component';
+import { NavBarSideComponent } from './nav-bar-side/nav-bar-side.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ProjectComponent } from './project/project.component';
     LoginComponent,
     RegisterComponent,
     DashboardComponent,
-    ProjectComponent
+    ProjectComponent,
+    NavBarSideComponent
   ],
   imports: [
     BrowserModule,
